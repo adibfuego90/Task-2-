5e2d17e44a00a9770327c264fa277b69af59f5b4
adibfuego90 <adiburraman11@gmail.com>
2021-10-06 22:02:39 +0600

    initial commit

---
+++ build/classes/adib/Main.java
@@ -0,0 +1,23 @@
+/*
+Name:Md.Adibur rahman 
+Id:2012020090
+Section:B
+Email:cse_2012020090@lus.ac.bd
+Date:16.07.2021
+*/
+package adib;
+
+public class Main {
+    public static void main(String[] args) {
+        
+       Student student=new Student();
+       
+       Student student1=new Student (2012020090);
+       
+       Student student2=new Student (2012020090,"Adib");
+       
+       student.display();
+       
+    }
+    
+}
